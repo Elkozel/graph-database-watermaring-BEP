8882085470aad3ffdef00b2748512c83d7ba1fe2
Tsvetomir Hristov <caci802@gmail.com>
2023-01-21 20:42:19 +0100

    Formatting

--- src/main.py
+++ src/main.py
@@ -54,15 +54,16 @@ def watermark_database(session,
     :param watermark_visibility: Optional. If selected, the letter W will be added on  the back of the type of the watermark document and edges, indicating that it is watermarked.
     """
     # Determine the size of each group
-    group_sizes = wk.get_random_set(len(ids), min_group_size, max_group_size, group_find_max_tries)
+    group_sizes = wk.get_random_set(
+        len(ids), min_group_size, max_group_size, group_find_max_tries)
     # Pick the groups, based on the predetermined size
     groups = wk.divide_groups(ids, group_sizes)
     # Generate pseudo document for each group
     document_ids = []
     for group in groups:
         pseudo_document = session.execute_read(
-            ps.create_pseudo_document, 
-            type=watermarked_document_type, 
+            ps.create_pseudo_document,
+            type=watermarked_document_type,
             fields=watermarked_document_fields,
             optional_fields=watermarked_document_optional_fields)
         # Embed watermark in each pseudo document
@@ -70,22 +71,22 @@ def watermark_database(session,
                            field=watermark_cover_field, fields=watermarked_document_fields)
         # Insert the pseudo documents inside the database
         pseudo_node = session.execute_write(db.create_node,
-            document=pseudo_document,
-            visible=watermark_visibility,
-            node_type=watermarked_document_type
-        )
-            
+                                            document=pseudo_document,
+                                            visible=watermark_visibility,
+                                            node_type=watermarked_document_type
+                                            )
+
         document_ids.append(pseudo_node)
         # Create relations
         for (node, label) in group:
             relation = RELATIONS[label]
             session.execute_write(db.create_relation,
-                source_id=pseudo_node,
-                dest_id=node,
-                edge_type=relation["type"],
-                reversed=(relation["dir"] == "in"),
-                visible=watermark_visibility
-            )
+                                  source_id=pseudo_node,
+                                  dest_id=node,
+                                  edge_type=relation["type"],
+                                  reversed=(relation["dir"] == "in"),
+                                  visible=watermark_visibility
+                                  )
     return document_ids
 
 
@@ -94,6 +95,7 @@ def get_all_company_ids(link):
                       "where not \"Company\" in labels(n)"
                       "return id(n), labels(n)[0]")
     return result.values()
+
 
 def watermark_uk_companies(session):
     """
@@ -104,29 +106,38 @@ def watermark_uk_companies(session):
     # Retrieve all IDs from the database
     all_ids = session.execute_read(get_all_company_ids)
     watermarked = watermark_database(session,
-                       ids=all_ids,
-                       min_group_size=10,
-                       max_group_size=1000,
-                       watermarked_document_type="Company",
-                       watermark_cover_field="companyNumber",
-                       watermarked_document_fields=["countryOfOrigin", "name", "status"],
-                       watermarked_document_optional_fields=["mortgagesOutstanding", "SIC", "category"],
-                       watermark_key=1,
-                       watermark_identity="RP Project",
-                       watermark_visibility=True)
+                                     ids=all_ids,
+                                     min_group_size=10,
+                                     max_group_size=1000,
+                                     watermarked_document_type="Company",
+                                     watermark_cover_field="companyNumber",
+                                     watermarked_document_fields=[
+                                         "countryOfOrigin", "name", "status"],
+                                     watermarked_document_optional_fields=[
+                                         "mortgagesOutstanding", "SIC", "category"],
+                                     watermark_key=1,
+                                     watermark_identity="RP Project",
+                                     watermark_visibility=True)
     return watermarked
-    
+
 
 def verify_watermark(session, watermarked_ids: List[int], key: int, watermark_identity: str, watermark_fields: List[str], watermark_cover_field: str, fast_check: bool = True):
     documents = session.execute_read(db.get_documents, ids=watermarked_ids)
     for document in documents:
-        result = wk.detect_watermark(document, key=key, identity=watermark_identity, field=watermark_cover_field, fields=watermark_fields)
+        result = wk.detect_watermark(document, key=key, identity=watermark_identity,
+                                     field=watermark_cover_field, fields=watermark_fields)
         if result:
             return True
     return False
 
+
 parser = argparse.ArgumentParser(description='Watermark a Neo4j database')
-parser.add_argument("-w", '--watermark', action='store_true', help='Watermark UK database without asking')
+parser.add_argument("-i", '--interactive', action='store_true',
+                    help='Show the interactive menu')
+parser.add_argument("-w", '--watermark', action='store_true',
+                    help='Watermark UK database without asking')
+parser.add_argument('--verify', action='store_true',
+                    help='Verify the watermark (exit with error if not verified)')
 # parser.add_argument('--reset', metavar='N', type=int,
 #                     help='Watermark UK database')
 
@@ -137,27 +148,42 @@ main_menu = [
     inquirer.List('Main menu',
                   message="What would you like me to do?",
                   choices=["Watermark UK database",
-                           "Populate database", "Reset --hard", "Exit"],
+                           "Verify watermark", "Populate database", "Reset --hard", "Exit"],
                   ),
 ]
+
+
+def show_menu():
+    # Present the user with the main menu
+    answer = inquirer.prompt(main_menu)
+    match answer["Main menu"]:
+        case "Watermark UK database":
+            with driver.session(database="neo4j") as session:
+                watermark_uk_companies(session)
+        case "Verify watermark":
+            with driver.session(database="neo4j") as session:
+                verify_watermark(session, [], 1, "", [], "")
+        case "Populate database":
+            with driver.session(database="neo4j") as session:
+                fake.populate_fake_data(session)
+        case "Reset --hard":
+            with driver.session(database="neo4j") as session:
+                db.delete_everythong(session)
+        case "Exit":
+            sys.exit(0)
+
 
 if __name__ == "__main__":
     with GraphDatabase.driver(URI, auth=AUTH) as driver:
         # Verify connection to the database
         driver.verify_connectivity()
-        
-        while True:
-            # Present the user with the main menu
-            answer = inquirer.prompt(main_menu)
-            match answer["Main menu"]:
-                case "Watermark UK database":
-                    with driver.session(database="neo4j") as session:
-                        watermark_uk_companies(session)
-                case "Populate database":
-                    with driver.session(database="neo4j") as session:
-                        fake.populate_fake_data(session)
-                case "Reset --hard":
-                    with driver.session(database="neo4j") as session:
-                        db.delete_everythong(session)
-                case "Exit":
-                    sys.exit(0)
+
+        if args.interactive:
+            while True:
+                show_menu()
+        elif args.watermark:
+            with driver.session(database="neo4j") as session:
+                watermark_uk_companies(session)
+        elif args.verify:
+            with driver.session(database="neo4j") as session:
+                verify_watermark(session)
