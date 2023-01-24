3d98a30c79ce221f2f9df99d9507e209906cb014
ELKozel <caci802@gmail.com>
2023-01-24 19:05:33 +0100

    Adding windows support

---
+++ setup.ps1
@@ -0,0 +1,15 @@
+﻿$runs = Read-Host -Prompt "Please specify the number of times to run: "
+
+echo "Building docker image"
+docker build -t rp/modification .
+
+for ( $s=0; $s -lt $runs; $s=$s + 1 ) {
+    echo "========== Run $s =========="
+    echo "Resetting data"
+    Remove-Item "./.db-data" -Recurse -Force
+    Expand-Archive -Path "./db/uk_companies.zip" -DestinationPath "."
+    echo "Composing docker container"
+    docker compose up --abort-on-container-exit # start the containers and wait until complete
+    echo "Disposing docker container"
+    docker compose down # burn everything
+}
