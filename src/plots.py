de81946c2aadbe7b271485cc54c1ccd5ddde9308
Tsvetomir Hristov <caci802@gmail.com>
2023-01-25 18:18:36 +0100

    Adding the ability to individually plot

--- src/plots.py
+++ src/plots.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import json
+import os
 
 def plot_documents_vs_time():
     data = ""
@@ -13,7 +14,7 @@ def plot_documents_vs_time():
     y1 = [datapoint["duration"] for datapoint in watermark_data]
 
     # plotting the line 1 points 
-    plt.plot(x1, y1, label = "Duration")
+    plt.plot(x1, y1)
     
     # naming the x axis
     plt.xlabel('Number of pseudo documents')
@@ -37,15 +38,25 @@ def plot_percDel_vs_numNodes():
     y1 = [datapoint["nodes_deleted"]/datapoint["nodes_before"] for datapoint in deletion_data]
 
     # plotting the line 1 points 
-    plt.plot(x1, y1, label = "Duration")
+    plt.plot(x1, y1)
     
     # naming the x axis
     plt.xlabel('Number of watermarked documents')
-    plt.ylim([0,1])
     # naming the y axis
+    plt.ylim([0,1])
     plt.ylabel('\% of nodes deleted')
     # giving a title to my graph
     plt.title('Watermark robustness')
     plt.legend()
 
     return plt
+
+# Run if you only want to generate the plots
+if __name__ == "__main__":
+    plot_dir = "plots/"
+    if not os.path.exists(plot_dir):
+        os.makedirs(plot_dir)
+    plot1 = plot_documents_vs_time()
+    plot1.savefig(os.path.join(plot_dir, "time_to_watermark.png"))
+    plot2 = plot_percDel_vs_numNodes()
+    plot2.savefig(os.path.join(plot_dir, "robustness.png"))
