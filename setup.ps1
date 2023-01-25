e6794b9044ee12ce82f8d4a56469338f672ef4f9
ELKozel <caci802@gmail.com>
2023-01-25 07:32:58 +0100

    Fixing windows PS script

--- setup.ps1
+++ setup.ps1
@@ -6,7 +6,10 @@ docker build -t rp/modification .
 for ( $s=0; $s -lt $runs; $s=$s + 1 ) {
     echo "========== Run $s =========="
     echo "Resetting data"
-    Remove-Item "./.db-data" -Recurse -Force
+    $db_data_dir = "./.db-data"
+    if (Test-Path $db_data_dir) {
+        Remove-Item $db_data_dir -Recurse -Force
+    }
     Expand-Archive -Path "./db/uk_companies.zip" -DestinationPath "."
     echo "Composing docker container"
     docker compose up --abort-on-container-exit # start the containers and wait until complete
